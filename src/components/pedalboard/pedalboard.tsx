136853fcb129652ca9b4eafdc941afb1768dec5e
webprofusion-chrisc <christopher.cook@webprofusion.com>
2023-01-09 15:13:48 +0800

    WIP: Visual FX

--- src/components/pedalboard/pedalboard.tsx
+++ src/components/pedalboard/pedalboard.tsx
@@ -13,12 +13,22 @@ import {
   Stars,
   ContactShadows,
   MeshReflectorMaterial,
+  Lightformer,
+  Effects,
+  AccumulativeShadows,
+  RandomizedLight,
 } from "@react-three/drei";
 import OverdriveBossMT2 from "./pedals/overdrive_boss_mt2";
 import AmpMesaDual from "./amps/amp_mesa_dual";
 import PhaserMXR90 from "./pedals/phaser_mxr_90";
 import GenericFXPedal from "./pedals/generic_pedal";
-import { Bloom, EffectComposer } from "@react-three/postprocessing";
+import {
+  Bloom,
+  DepthOfField,
+  EffectComposer,
+  Noise,
+  Vignette,
+} from "@react-three/postprocessing";
 import { MeshStandardMaterial, Vector3 } from "three";
 import { CuboidCollider, Debug, Physics, RigidBody } from "@react-three/rapier";
 
@@ -29,7 +39,21 @@ const PedalboardControl = () => {
       shadows
       camera={{ position: [0, 0, 35], fov: 25 }}
     >
-      <fog attach="fog" args={["black", 15, 50]} />
+      <color attach="background" args={["#15151a"]} />
+      {/*<color attach="background" args={["#f0f0f0"]} />*/}
+      <ambientLight intensity={0.3} />
+      <pointLight position={[0, 5, 25]} intensity={1.5} />
+      <Environment resolution={32}>
+        <Lightformer position={[10, 10, 10]} scale={10} intensity={4} />
+        <Lightformer
+          position={[10, 0, -10]}
+          scale={10}
+          color="red"
+          intensity={6}
+        />
+        <Lightformer position={[-10, -10, -10]} scale={10} intensity={4} />
+      </Environment>
+
       <Stars
         radius={100}
         depth={50}
@@ -39,54 +63,60 @@ const PedalboardControl = () => {
         fade
         speed={1}
       />
-      <Stage
-        intensity={0.1}
-        environment="studio"
-        shadows={{ type: "accumulative", bias: -0.001 }}
-        adjustCamera={false}
+
+      <Physics colliders="hull" gravity={[0, -4, 0]}>
+        <RigidBody>
+          <GenericFXPedal position={[-10, 5, 0]} />
+        </RigidBody>
+
+        <RigidBody>
+          <PhaserMXR90 position={[-5, 5, 0]} />
+        </RigidBody>
+
+        <RigidBody restitution={0.1}>
+          <OverdriveBossMT2 position={[0, 3, 0]} />
+        </RigidBody>
+
+        <RigidBody restitution={0.2} colliders="trimesh">
+          <AmpMesaDual position={[10, 5, 0]} />
+        </RigidBody>
+
+        {/* Floor */}
+        <CuboidCollider
+          position={[0, -5, 0]}
+          args={[40, 1, 20]}
+          restitution={0.1}
+        />
+        {/*<Debug/> */}
+
+        <ContactShadows
+          scale={20}
+          blur={0.6}
+          opacity={0.2}
+          position={[0, -4, 0]}
+        />
+      </Physics>
+
+      <AccumulativeShadows
+        temporal
+        frames={Infinity}
+        alphaTest={1}
+        blend={200}
+        limit={1500}
+        scale={25}
+        position={[0, -0.05, 0]}
       >
-        <Suspense>
-          <Physics colliders="hull">
-            <RigidBody>
-              <GenericFXPedal position={[-10, 5, 0]} />
-            </RigidBody>
+        <RandomizedLight
+          amount={1}
+          mapSize={512}
+          radius={5}
+          ambient={0.5}
+          position={[-10, 10, 15]}
+          size={10}
+          bias={0.001}
+        />
+      </AccumulativeShadows>
 
-            <RigidBody>
-              <PhaserMXR90 position={[-5, 5, 0]} />
-            </RigidBody>
-
-            <RigidBody restitution={0.1}>
-            <OverdriveBossMT2 position={[0, 5, 0]} />
-            </RigidBody>
-
-            <RigidBody restitution={0.2} colliders="trimesh">
-              <AmpMesaDual position={[10, 5, 0]} />
-            </RigidBody>
-
-            {/* Floor */}
-            <CuboidCollider position={[0, -5, 0]} args={[40, 1, 20]} />
-            {/*<Debug/> */}
-            
-            <ContactShadows
-              scale={20}
-              blur={0.6}
-              opacity={0.2}
-              position={[0, -4, 0]}
-            />
-          </Physics>
-        </Suspense>
-      </Stage>
-
-      {/* <Grid
-        renderOrder={-1}
-        position={[0, -1.85, 0]}
-        infiniteGrid
-        cellSize={0.6}
-        cellThickness={0.6}
-        sectionSize={3.3}
-        sectionThickness={1.5}
-        fadeDistance={30}
-  /> */}
       <OrbitControls
         autoRotate
         autoRotateSpeed={0.05}
@@ -95,10 +125,11 @@ const PedalboardControl = () => {
         minPolarAngle={Math.PI / 2}
         maxPolarAngle={Math.PI / 2}
       />
-      <EffectComposer disableNormalPass>
-        <Bloom luminanceThreshold={1} mipmapBlur />
+
+      <EffectComposer>
+        <Noise opacity={0.02} />
+        <Vignette eskil={false} offset={0.1} darkness={1.1} />
       </EffectComposer>
-      <Environment background preset="sunset" blur={0.8} />
     </Canvas>
   );
 };
